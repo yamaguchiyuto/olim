93c3dbe5c3c6fed337532952a96c6df8c44352a9
yamaguchiyuto <yuto.ymgc@gmail.com>
2014-05-28 10:35:16 -0400

    Modified to be able to handle user assign

--- lib/quadtree.py
+++ lib/quadtree.py
@@ -1,6 +1,80 @@
 # -*- coding: utf-8 -*-
 import sys
 
+class Quadtree:
+    def __init__(self,data,x1,y1,x2,y2):
+        self.data = data
+        self.areas = self.init_area(data,x1,y1,x2,y2)
+        self.assign = self.init_assign(data)
+
+    def init_assign(self,data):
+        assign = {}
+        for uid in data:
+            assign[uid] = -1
+        return assign
+
+    def init_area(self,data,x1,y1,x2,y2):
+        initial = Area(x1,y1,x2,y2,0)
+        for uid in data:
+            initial.append(data[uid])
+        return [initial]
+
+    def covered(self,uid):
+        return self.assign[uid]
+
+    def divide(self,area,level):
+        division = []
+
+        """ 分割後の各辺の長さ """
+        xl = (area.x2 - area.x1)/2
+        yl = (area.y2 - area.y1)/2
+
+        """ 分割後の領域を生成 """
+        for dx in [0,1]:
+            for dy in [0,1]:
+                sub_area = Area(area.x1+dx*xl, area.y1+dy*yl, area.x1+(1+dx)*xl, area.y1+(1+dy)*yl,level)
+                division.append(sub_area)
+
+        """ 分割前の領域に属すデータ点を分割後の領域にアサイン """
+        for p in area.points():
+            for sub_area in division:
+                if sub_area.cover(p):
+                    sub_area.append(p)
+                    break
+
+        return division
+
+    def quadtree(self, maxpoints, maxdivision):
+        """ 引数で与えられたmaxdivision回だけ分割を繰り返す """
+        for n in range(maxdivision):
+            new_areas = []
+            for i in range(len(self.areas)):
+                if not self.areas[i].is_fixed():
+                    """ まだ分割が終わっていない領域に対して """
+                    if self.areas[i].number_of_points() > maxpoints:
+                        """ 領域に属すデータ点の数がmaxpoints個を超えていたら分割 """
+                        division = self.divide(self.areas[i],n+1)
+                        for d in division:
+                            new_areas.append(d)
+                    else:
+                        """ 領域に属すデータ点の数がmaxpoints個を超えていなかったらもう分割しない """
+                        self.areas[i].set_fixed()
+                        new_areas.append(self.areas[i])
+                else:
+                    """ 分割が終わっていればそのまま """
+                    new_areas.append(self.areas[i])
+            self.areas = new_areas
+
+        """ データ点がどのエリアにアサインされているかのインデックスを作る """
+        self.make_assign()
+
+    def make_assign(self):
+        for uid in self.data:
+            for i in range(len(self.areas)):
+                if self.areas[i].cover(data[uid]):
+                    break
+            self.assign[uid] = i
+
 class Area:
     def __init__(self,x1,y1,x2,y2,level):
         self.x1 = x1
@@ -19,6 +93,9 @@ class Area:
         """ 領域に属しているデータ点を返す """
         return self.points_
 
+    def number_of_points(self):
+        return len(self.points_)
+
     def is_fixed(self):
         """ 分割が終わっているかどうか """
         return self.fixed
@@ -34,60 +111,14 @@ class Area:
         else:
             return False
 
-def divide(area,level):
-    division = []
-
-    """ 分割後の各辺の長さ """
-    xl = (area.x2 - area.x1)/2
-    yl = (area.y2 - area.y1)/2
-
-    """ 分割後の領域を生成 """
-    for dx in [0,1]:
-        for dy in [0,1]:
-            sub_area = Area(area.x1+dx*xl, area.y1+dy*yl, area.x1+(1+dx)*xl, area.y1+(1+dy)*yl,level)
-            division.append(sub_area)
-
-    """ 分割前の領域に属すデータ点を分割後の領域にアサイン """
-    for p in area.points():
-        for sub_area in division:
-            if sub_area.cover(p):
-                sub_area.append(p)
-                break
-
-    return division
-
-
-def quadtree(data, initial, maxpoints, maxdivision):
-    areas = [initial]
-
-    """ 引数で与えられたmaxdivision回だけ分割を繰り返す """
-    for n in range(maxdivision):
-        new_areas = []
-        for i in range(len(areas)):
-            if not areas[i].is_fixed():
-                """ まだ分割が終わっていない領域に対して """
-                if len(areas[i].points()) > maxpoints:
-                    """ 領域に属すデータ点の数がmaxpoints個を超えていたら分割 """
-                    division = divide(areas[i],n+1)
-                    for d in division:
-                        new_areas.append(d)
-                else:
-                    """ 領域に属すデータ点の数がmaxpoints個を超えていなかったらもう分割しない """
-                    areas[i].set_fixed()
-                    new_areas.append(areas[i])
-            else:
-                """ 分割が終わっていればそのまま """
-                new_areas.append(areas[i])
-        areas = new_areas
-
-    return areas
-
-
 def read_data(file_name):
-    data = []
+    data = {}
     for line in open(file_name, 'r'):
-        p = tuple([float(v) for v in line.rstrip().split(' ')])
-        data.append(p)
+        entries = line.rstrip().split(' ')
+        uid = int(entries[0])
+        lat = float(entries[1])
+        lng = float(entries[2])
+        data[uid] = (lat,lng)
     return data
 
 if __name__ == '__main__':
@@ -99,17 +130,16 @@ if __name__ == '__main__':
     maxdivision = int(sys.argv[6])
     data = read_data(sys.argv[7])
 
-    """ 対象とする領域を生成 """
-    initial = Area(x1,y1,x2,y2,0)
-    for d in data:
-        initial.append(d)
-
     """ 分割 """
-    qtree = quadtree(data, initial, maxpoints, maxdivision)
+    qtree = Quadtree(data,x1,y1,x2,y2)
+    qtree.quadtree(maxpoints, maxdivision)
 
     """ 結果 """
-    for a in qtree:
+    for a in qtree.areas:
         print "%s %s %s %s" % (a.x1, a.y1, a.x2, a.y2),
         for p in a.points():
             print p,
         print
+
+    for uid in data:
+        print uid,qtree.covered(uid)
